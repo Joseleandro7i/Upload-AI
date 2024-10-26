3a6589ba047a9f71a9edb32dfc96670e3ea180b6
Jose-leandro <lenadrojose124leandro@gmail.com>
2024-10-26 08:51:15 -0300

    feat: install and configure Angular for Upload AI project
    This change replaces React and Vite with Angular. The creation of a new, separate Angular project was necessary due to the complexity of integrating Angular into the existing codebase. This setup is part of my goal to learn and master Angular technology.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'upload-ai-frontend' title`, () => {
+  it(`should have the 'upload-ai' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('upload-ai-frontend');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, upload-ai-frontend');
+    expect(app.title).toEqual('upload-ai');
   });
 });
