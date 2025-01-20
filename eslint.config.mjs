5e78b4c5b36e55c5129dc1cf214405a8b6f2245f
Jose-leandro <lenadrojose124leandro@gmail.com>
2025-01-20 13:49:08 -0300

    Configuration of eslint chore: set up ESLint configuration
    - Configured ESLint to enforce code quality and maintain consistent coding practices.
    
    - Included relevant  informations as part of the setup.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,26 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import { defineConfig } from "eslint-define-config"; // Importing the defineConfig function
 
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts,tsx}"], // Include .tsx for React
+    languageOptions: {
+      globals: globals.browser,
+      parser: "@typescript-eslint/parser", // Specify the parser for TypeScript
+      parserOptions: {
+        ecmaVersion: 2020, // or whatever version you prefer
+        sourceType: "module",
+      },
+    },
+    extends: [
+      'plugin:@typescript-eslint/recommended', 
+    ],
 
-export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
+    plugins: {},
+    rules: {},
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-];
+]);
